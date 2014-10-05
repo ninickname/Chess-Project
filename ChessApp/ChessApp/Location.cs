569560722fb243ca4466641cc3c3ac30673ac5ab
ninickname <yashaev@gmail.com>
2014-10-05 04:32:44 +0300

    first part , just the basics

---
+++ ChessApp/ChessApp/Location.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessApp
+{
+    class Location
+    {
+        char x;
+        int y;
+
+        Location(char x, int y)
+        {
+            setLocation(x, y);
+        }
+
+        void setLocation(char x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+
+
+
+
+    }
+}
---
+++ ChessApp/ChessApp/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessApp
+{
+    class Player
+    {
+        public static bool UP = true;
+        public static bool down = false;
+
+        public List<Figure> figures;
+        public bool direction;
+            
+        Player(bool direction){    
+            this.figures = new List<Figure>();
+            this.direction = direction;
+        }
+
+    }
+    
+}
